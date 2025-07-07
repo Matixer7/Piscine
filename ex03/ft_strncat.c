c14bd6616e1826ed69ead79547444be3fbcc8fc8
Matixer7 <mateusznr7@gmail.com>
2025-07-07 19:11:00 +0200

    forgot to comment main

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 10:20:24 by mgumienn          #+#    #+#             */
-/*   Updated: 2025/07/07 18:51:08 by marvin           ###   ########.fr       */
+/*   Updated: 2025/07/07 19:10:47 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	dest[i] = '\0';
 	return (dest);
 }
-int	main(void) {
+/*int	main(void) {
 	char	source[] = " World";
 	char	destination[] = "Hello";
 	char	*src = source;
@@ -45,4 +45,4 @@ int	main(void) {
 		i++;
 	}
 	
-}
+}*/
