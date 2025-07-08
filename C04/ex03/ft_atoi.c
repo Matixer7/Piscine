9ab0411a31f937e051332abd41fd0f62c0c20b3c
Matixer7 <mateusznr7@gmail.com>
2025-07-08 19:41:36 +0200

    ????

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,40 +1,43 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int    odd_or_even(char *str)
+int	odd_or_even(char *str)
 {
-    int i;
-    int j;
-    
-    i = 0;
-    j = 0;
-    while (str[i] && (str[i] == '+'  || str[i] == '-'))
-    {
-        if (str[i] == '-')
-            j++;
-        i++;
-    }
-    return (j % 2);
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (str[i] && (str[i] == '+'  || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			j++;
+		i++;
+	}
+	return (j % 2);
 }
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 { 
-    int i;
+	int	i;
+	int	n;
 
-    i = 0;
-    if (odd_or_even(str) == 1)
-        write(1, "-", 1);
-    while (str[i] == '-' || str[i] == '+')
-        i++;
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	n = 0;
+	while (str[i] == '-' || str[i] == '+')
+	i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - 48);
+		i++;
+	}
+	if (odd_or_even(str) == 1)
+		n = -n;
+	return (n);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    ft_atoi(argv[1]);
-    return argc;
+	printf("%d", (argv[1]));
+	return argc;
 }
