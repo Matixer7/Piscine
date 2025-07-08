66fb876f136c7f676f48da71940dd7d14a9dbc7d
Matixer7 <mateusznr7@gmail.com>
2025-07-08 18:31:47 +0200

    ft_putnbr fixed

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -13,22 +13,24 @@ void    int_converter(int n, char *str)
     if ((n / 10) > 0)
         int_converter(n / 10, str);
     str[i++] = (n % 10) + '0';
+    str[i] = '\0';
 }
 void    ft_putnbr(int nb)
 {
     int     x;
-    char    *str;
+    char    str[11];
+    char        *p;
 
+    p = str;
     x = 0;
-    int_converter(nb, str);
+    int_converter(nb, p);
     while (str[x])
         x++;
-    x++;
     write(1, str, x);
 }
 
 /*int main(void)
 {
-    ft_putnbr(-425);
+    ft_putnbr(-2147483648);
     return 0;
 }*/
