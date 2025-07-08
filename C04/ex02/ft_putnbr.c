83a56a7de1dabc4e33f17efe9d9036fcf031d050
Matixer7 <mateusznr7@gmail.com>
2025-07-08 18:53:25 +0200

    test of ft_atoi

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -31,6 +31,6 @@ void    ft_putnbr(int nb)
 
 /*int main(void)
 {
-    ft_putnbr(-2147483648);
+    ft_putnbr(-2147483647);
     return 0;
 }*/
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+int    odd_or_even(char *str)
+{
+    int i;
+    int j;
+    
+    i = 0;
+    j = 0;
+    while (str[i] && (str[i] == '+'  || str[i] == '-'))
+    {
+        if (str[i] == '-')
+            j++;
+        i++;
+    }
+    return (j % 2);
+}
+
+int ft_atoi(char *str)
+{ 
+    int i;
+
+    i = 0;
+    if (odd_or_even(str) == 1)
+        write(1, '-', 1);
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        write(1, str[i], 1);
+        i++;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    ft_atoi(argv[1]);
+    return 0;
+}
